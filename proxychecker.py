3bbdc4927d68e8489b68177f389d8160887155b5
amani <amanitoama570@gmail.com>
2024-11-09 00:17:19 +0200

    change banner,make the result in a table if the input is file, add country

--- proxychecker.py
+++ proxychecker.py
@@ -2,6 +2,7 @@
 # coding: utf-8
 # PROXY Checker v0.1.0
 # By Amani Toama amanitoama570@gmail.com
+from itertools import count
 
 # modules in standard library
 import requests
@@ -10,6 +11,8 @@ import re
 import argparse
 from requests.exceptions import ProxyError, ConnectTimeout
 from colorama import Fore, Style, init
+import sys
+import  signal
 
 
 def initialize():
@@ -24,28 +27,39 @@ def initialize():
     args = parser.parse_args()
     return args
 
-
+# ctrl + c interrupting
+def signal_handler(sig, frame):
+    print(f'\nYou pressed Ctrl+C! Exiting Code 0 \n')
+    # Perform any cleanup if needed
+    sys.exit(0)
 def print_banner():
     print(rf"""
       {Fore.LIGHTMAGENTA_EX}
-************************************************************************
-                     
-____________ _______   ___   __  _____ _               _             
-| ___ \ ___ \  _  \ \ / | \ / / /  __ \ |             | |            
-| |_/ / |_/ / | | |\ V / \ V /  | /  \/ |__   ___  ___| | _____ _ __ 
-|  __/|    /| | | |/   \  \ /   | |   | '_ \ / _ \/ __| |/ / _ \ '__|
-| |   | |\ \\ \_/ / /^\ \ | |   | \__/\ | | |  __/ (__|   <  __/ |   
-\_|   \_| \_|\___/\/   \/ \_/    \____/_| |_|\___|\___|_|\_\___|_|   
-                                                                     
-                                                                     
- *************************************************************************
- {Style.BRIGHT + Fore.CYAN}
-# Proxy Checker Tool by Amani Toama  amanitoama570@gmail.com
- --------------------------------------------------------------------------
- 
- {Fore.WHITE}Checking Proxy... 
-      """)
 
+
+  _____   ______  _____  _     _ __   __      _______ _     _ _______ _______ _     _ _______  ______
+ |_____] |_____/ |     |  \___/    \_/        |       |_____| |______ |       |____/  |______ |_____/
+ |       |    \_ |_____| _/   \_    |         |_____  |     | |______ |_____  |    \_ |______ |    \_
+                                                                                                     
+
+ 
+{Style.DIM}{Fore.CYAN}# Proxy Checker Tool by Amani Toama  amanitoama570@gmail.com
+ --------------------------------------------------------------------------
+ """)
+
+# extract ip func
+def extract_ip(address):
+    ip = address.split(':')[0]
+    return ip
+
+# country detective func
+def get_ip_country(ip):
+    res = requests.get(f"http://ip-api.com/json/{ip}")
+    data = res.json()
+    if res.status_code == 200:
+        return data['country']
+    else:
+        return "error"
 
 def check_proxy(proxy, proxy_type):
     proxies = {proxy_type: proxy}
@@ -73,36 +87,58 @@ def identify_proxy_type(proxy):
     else:
         return 'http'
 
+print_banner()
 
 def main(proxy_file_path=None, single_proxy=None, protocol=None):
-    print_banner()
+    signal.signal(signal.SIGINT, signal_handler)
+    print(f"{Style.RESET_ALL}{Fore.WHITE}Checking Proxy... ")
+    print(rf"""{Fore.YELLOW}
+   ___________________________________________________________________________________________
+                PROXY              |       Latency       |    Status    |      Country       
+  --------------------------------------------------------------------------------------------
+                          """)
     try:
         if proxy_file_path:
             with open(proxy_file_path, 'r') as file:
                 proxies = file.readlines()
             for i, proxy in enumerate(proxies):
                 proxy = proxy.strip()
+                ip = extract_ip(proxy)
+                cntry = get_ip_country(ip)
+
                 if proxy:
                     status, latency = check_proxy(proxy, identify_proxy_type(proxy))
                     if status:
-                        print(f"{Fore.GREEN}Proxy {proxy} is working. Latency: {latency:.2f} seconds.")
+                        print(
+                            f"{Fore.GREEN}  {proxy:^32} |     {latency:.2f}sec         |    {'True' if status else 'False':<9} | {cntry:^15}  ")
                     else:
-                        print(f"{Fore.RED}Proxy {proxy} is not working.")
-                if (i + 1) % 10 == 0:  # Every 3 proxies, ask to continue or terminate
+                        print(
+                            f"{Fore.RED}  {proxy:^32} | {'---':^17}   |    {'True' if status else 'False':<9} | {cntry:^15}  ")
+                if (i + 1) % 10 == 0:  # Every 10 proxies, ask to continue or terminate
                     if input(f"{Fore.BLUE}Press 'q' to quit or any other key to continue: ").lower() == 'q':
                         print(f"{Fore.YELLOW}Proxy checking terminated by user.")
                         break
         elif single_proxy:
             protocol_type = protocol if protocol else identify_proxy_type(single_proxy)
+            cntry = get_ip_country(extract_ip(single_proxy))
             status, latency = check_proxy(single_proxy, protocol_type)
             if status:
-                print(f"{Fore.GREEN}Proxy {single_proxy} is working. Latency: {latency:.2f} seconds.")
+                print(f"{Fore.GREEN}Proxy {single_proxy} is working. Latency: {latency:.2f} seconds. Country: {cntry}")
             else:
-                print(f"{Fore.RED}Proxy {single_proxy} is not working.")
+                print(
+                    f"{Fore.RED}  {single_proxy:^32} | {'---':^17}   |    {'True' if status else 'False':<9} | {cntry:^15}  ")
         else:
-            print(f"{Fore.RED}No proxy information provided.")
+            print(f"{Fore.RED}Proxy {single_proxy} is not working.")
     except FileNotFoundError:
         print(f"{Fore.RED}File {proxy_file_path} not found.")
+
+    try:
+        while True:
+            pass
+    except KeyboardInterrupt:
+        # Catch KeyboardInterrupt to ensure the program exits gracefully
+        print('\nExiting due to Ctrl+C')
+        sys.exit(0)
 
 
 if __name__ == "__main__":
@@ -113,3 +149,4 @@ if __name__ == "__main__":
         main(single_proxy=args.single, protocol=args.protocol)
     elif args.txt:
         main(proxy_file_path=args.txt)
+
